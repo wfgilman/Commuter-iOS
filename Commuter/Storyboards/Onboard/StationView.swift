7b8228738ccb572eb05ffa2756a976d25fca2b63
Will Gilman <will@samadhi.tech>
2019-03-08 06:33:05 -0800

    Added new font and started work on Picker view cell.

---
+++ Commuter/Storyboards/Onboard/StationView.swift
@@ -0,0 +1,21 @@
+//
+//  StationView.swift
+//  Commuter
+//
+//  Created by Will Gilman on 2/28/19.
+//  Copyright © 2019 BGHFM. All rights reserved.
+//
+
+import UIKit
+
+class StationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
