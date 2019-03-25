5cfe7e21455e3603773039fa1b831d364528cc95
Will Gilman <will@samadhi.tech>
2019-03-25 07:07:41 -0700

    Created StationView for picker.

--- Commuter/Storyboards/Onboard/StationView.swift
+++ Commuter/Storyboards/Onboard/StationView.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2019 BGHFM. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StationView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var topDot1View: UIView!
+    @IBOutlet weak var topDot2View: UIView!
+    @IBOutlet weak var topDot3View: UIView!
+    @IBOutlet weak var markerView: UIView!
+    @IBOutlet weak var bottomDot1View: UIView!
+    @IBOutlet weak var bottomDot2View: UIView!
+    @IBOutlet weak var bottomDot3View: UIView!
+    @IBOutlet weak var stationLabel: UILabel!
+    
+    var station: Station! {
+        didSet {
+            stationLabel.text = station.name
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        initSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSubviews()
+    }
+    
+    func initSubviews() {
+        let nib = UINib(nibName: "StationView", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        contentView.frame = bounds
+        addSubview(contentView)
+        setupView()
+    }
+    
+    func setupView() {
+        markerView.layer.cornerRadius = 5
+        topDot1View.layer.cornerRadius = 1.5
+        topDot2View.layer.cornerRadius = 1.5
+        topDot3View.layer.cornerRadius = 1.5
+        bottomDot1View.layer.cornerRadius = 1.5
+        bottomDot2View.layer.cornerRadius = 1.5
+        bottomDot3View.layer.cornerRadius = 1.5
+    }
+}
